522ef8944df1e042b983d20976bab368491fd26c
lelecatica <malanhuk.leticia@escola.pr.gov.br>
2026-05-14 12:55:47 -0300

    ´

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ textos[i].classList.add("ativo");
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 
-contadores[0].textContent = calculaTempo tempoObjetivo1
+contadores[0].textContent = calculaTempo(tempoObjetivo1)
 
 function calculaTempo(tempoObjetivo){
      let tempoAtual = new Date();
